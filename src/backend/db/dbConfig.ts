dda73fd18804c8e6081c515e74040c7e9b783d5b
Hugh Henry <hughcthenry@gmail.com>
2022-12-08 16:42:14 +1100

    chore: Remove unnused code

--- src/backend/db/dbConfig.ts
+++ src/backend/db/dbConfig.ts
@@ -1,12 +1,4 @@
 export const dbConfig = {
-  // The number of realtime db event updates per second
-  defaultEventsPerSecond: 5,
-  realtime: {
-    broadcast: {
-      // The interval between broadcasts as a publisher on realtime db, in ms
-      defaultBroadcastPublishInterval: 200,
-    },
-  },
   channels: {
     meetings: {
       schema: 'public',
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -2,8 +2,3 @@ export const validateUsername = (username: string) => {
   // TODO: username validity function
   return username !== '';
 };
-
-export const validateRoomName = (roomName: string) => {
-  // TODO: roomName validity function
-  return roomName !== '';
-};
