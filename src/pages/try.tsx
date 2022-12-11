483d58663fb9167eabad2f38cd56b8ba5ec34327
Hugh Henry <hughcthenry@gmail.com>
2022-12-11 15:25:03 +1100

    chore: Remove line schedulor test for try page

--- src/pages/try.tsx
+++ src/pages/try.tsx
@@ -1,49 +1,7 @@
 import { Stack } from '@mui/material';
-import { addDays, setHours } from 'date-fns';
-import { range } from 'lodash';
-import { useState } from 'react';
-
-import LineSchedulor from '@/components/Schedulor/LineSchedulor';
-import type { SchedulorSelection } from '@/sharedTypes';
 
 const Index = () => {
-  const numDays = 5;
-  const schedulorStartDate = new Date('2022-12-08');
-  const schedulorEndDate = addDays(schedulorStartDate, numDays);
-  const minTime = 17;
-  const maxTime = 23;
-  const intervalSize = 1;
-
-  const [selections, setSelections] = useState<SchedulorSelection[]>(
-    range(0, numDays).map((i) => {
-      const day = addDays(schedulorStartDate, i);
-      const startDate = setHours(day, minTime);
-      const endDate = setHours(startDate, maxTime);
-      const selection: SchedulorSelection = {
-        startDate,
-        endDate,
-      };
-      return selection;
-    })
-  );
-
-  const onChange = (newSelections: SchedulorSelection[]) => {
-    setSelections(newSelections);
-  };
-
-  return (
-    <Stack sx={{ gap: 4 }}>
-      <LineSchedulor
-        selections={selections}
-        onChange={onChange}
-        startDate={schedulorStartDate}
-        endDate={schedulorEndDate}
-        minTime={minTime}
-        maxTime={maxTime}
-        intervalSize={intervalSize}
-      />
-    </Stack>
-  );
+  return <Stack sx={{ gap: 4 }}></Stack>;
 };
 
 export default Index;
