4104e6d3cc8920c2b4ba864bdaa4dd3db2fa0498
Hugh Henry <hughcthenry@gmail.com>
2022-12-16 13:06:32 +1100

    chore: Update alert context

--- src/context/Alert/alertContext.tsx
+++ src/context/Alert/alertContext.tsx
@@ -5,13 +5,15 @@ import { AlertType, UIAlert } from '@/types/uiAlert';
 
 export type UIAlertContextType = {
   alert: UIAlert;
-  dispatchAlert: (alert: UIAlert) => void;
+  dispatchAlert: (type: AlertType, message: string, open?: boolean) => void;
+  dispatchErrorAlert: (message: string, open?: boolean) => void;
   closeAlert: () => void;
 };
 
 const UIAlertContextInitialValue: UIAlertContextType = {
   alert: new UIAlert(AlertType.ERROR, '', false),
-  dispatchAlert: (_) => {},
+  dispatchAlert: (_, __, ___) => {},
+  dispatchErrorAlert: (_, __) => {},
   closeAlert: () => {},
 };
 
@@ -24,7 +26,17 @@ export type UIAlertProviderProps = { children: ReactNode };
 const UIAlertProvider = ({ children }: UIAlertProviderProps) => {
   const [alert, setAlert] = useState<UIAlert>(UIAlertContextInitialValue.alert);
 
-  const dispatchAlert = (newAlert: UIAlert) => {
+  const dispatchErrorAlert = (message: string, open: boolean = true) => {
+    const newAlert = new UIAlert(AlertType.ERROR, message, open);
+    setAlert(newAlert);
+  };
+
+  const dispatchAlert = (
+    type: AlertType,
+    message: string,
+    open: boolean = true
+  ) => {
+    const newAlert = new UIAlert(type, message, open);
     setAlert(newAlert);
   };
 
@@ -33,7 +45,9 @@ const UIAlertProvider = ({ children }: UIAlertProviderProps) => {
   };
 
   return (
-    <UIAlertContext.Provider value={{ alert, dispatchAlert, closeAlert }}>
+    <UIAlertContext.Provider
+      value={{ alert, dispatchAlert, dispatchErrorAlert, closeAlert }}
+    >
       {children}
     </UIAlertContext.Provider>
   );
